ac763a8529dd5131ea21d8c9a17a3e9176258e56
ismailr <ismailr@computicket.com>
2022-05-05 22:43:06 +0200

    Added additional logic to Buddies interactor

--- Investec/BuddiesInteractor.cs
+++ Investec/BuddiesInteractor.cs
@@ -15,20 +15,28 @@ namespace Investec
         public Task<IEnumerable<Buddy>> GetBuddies(List<Actors> actors)
         {
             var friendsByMovie = new Dictionary<string,List<string>>();
+            var buddies = new List<Buddy>();
            
             foreach (var actor in actors) {
-                
-                var filmList = actor.Films.GroupBy(x => x.Name).Select(y => y.First());
-
+                //I have a list of actors who acted in movies 
+                //need to select all the people who have acted in the same movie 
+                var filmList = actor.Films.GroupBy(x => x.Name).Select(f => f.First());
+                var actorList = new List<string>();
                 foreach (var film in filmList) {
+                    if (!friendsByMovie.ContainsKey(film.Name)) {
+                        var actorsByfilm = (from a in actors
+                                            from f in a.Films
+                                            where f.Name == film.Name
+                                            select a.Name).ToList();
 
-                    var actorList = (from a in actors
-                                     from f in a.Films
-                                     where f.Name == film.Name
-                                     select a.Name).ToList();
-                };
+                        friendsByMovie.Add(film.Name, actorsByfilm);
+                        }
+                    };
+                
+                }
+                
             }
-            return null;
+            //return null;
         }
     }
 }
